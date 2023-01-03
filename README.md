8516e25f50733833131671c742b8667ee990e780
unknown <amosehiguese@gmail.com>
2023-01-03 14:45:36 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+## Accounts
+
+This is a sample user account setup using the django-rest-authemail 3rd party package.
--- accounts/settings.py
+++ accounts/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "users", 
-    # "knox",
     "authemail",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -145,7 +144,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
-        # "knox.auth.TokenAuthentication"
     )
 }
 
